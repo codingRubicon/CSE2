971518fe1f7f3f05bd90cd790c4a36229607eaa4
codingrubicon <dek218@lehigh.edu>
2014-08-29 15:40:11 +0000

    finished hwk 1

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Dalyan Kosar
+//Welcome Class Java Program
+//
+//
+// Display a welcome screen with my network ID as a signature
+//
+//
+public class WelcomeClass{
+    
+    public static void main(String[] args) {
+        System.out.println("   ---------------  ");
+        System.out.println("    |  WELCOME  |   ");
+        System.out.println("   ---------------  ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-D--E--K--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("Before leaving for college, my friends and I");
+        System.out.println("went on a camping trip for the weekend.");
+    }
+}
